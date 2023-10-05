d78226a12a74cd7b83c65419568b406999cdd2bf
agustinsilva20 <agustinsilvab@hotmail.com>
2023-10-05 01:34:56 -0300

    al click afuera del modal se cierra

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -6,10 +6,10 @@ class Modal extends Component {
   render() {
     return (
       
-           <div className='modal-background'>
+           <div className='modal-background' onClick={this.props.setModal.bind(false, null)}>
                 <div className="modal">
                     <br />
-                    <h5 className='exit-modal' onClick={this.props.setModal.bind("",false, null)}><span>Exit</span></h5>
+                    <h5 className='exit-modal' onClick={this.props.setModal.bind(false, null)}><span>Exit</span></h5>
                     {this.props.elem}
                 </div>
            </div>
